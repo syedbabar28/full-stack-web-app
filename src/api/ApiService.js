f154dbe96849a0aff3c6842ac750c767361bb427
syedbabar09 <babar.khursheed@m3tech.com.pk>
2023-12-27 12:34:09 +0500

    api endpoint changes

--- src/api/ApiService.js
+++ src/api/ApiService.js
@@ -1,25 +1,25 @@
 import { ApiClient } from "./ApiClient";
 
 export const testApi = () => {
-    return ApiClient.get('/getAllTodos')
+    return ApiClient.get('/todos/getAllTodos')
 }
 
 export const getAllTodos = () => {
-    return ApiClient.get('/getAllTodos')
+    return ApiClient.get('/todos/getAllTodos')
 }
 
 export const deleteTodo = (id) => {
-    return ApiClient.get(`/deleteTodo/${id}`)
+    return ApiClient.get(`/todos/deleteTodo/${id}`)
 }
 
 export const addTodo = (todo) => {
-    return ApiClient.post('/addTodo', todo)
+    return ApiClient.post('/todos/addTodo', todo)
 }
 
 export const updateTodo = (id, todo) => {
-    return ApiClient.post(`/updateTodo/${id}`, todo)
+    return ApiClient.post(`/todos/updateTodo/${id}`, todo)
 }
 
 export const getTodoById = (id) => {
-    return ApiClient.get(`/getTodo/${id}`)
+    return ApiClient.get(`/todos/getTodo/${id}`)
 }
